fb0ed6d87719d9e2ffba0effae2cda5382e4040f
Peter McNulty <67034896+Petemc121@users.noreply.github.com>
2021-03-12 14:42:38 +0900

    Update wordsearch.js

--- wordsearch.js
+++ wordsearch.js
@@ -2,31 +2,41 @@
 var container = document.getElementById('wordsearch-container');
 var makeButton = document.getElementById('make')
 var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-// var wordArray = wordListGen();
+
 
 
 make.addEventListener('click', function() {
 
     var sizeInput = document.getElementById('sizeInput').value
-
-    if (isNaN(sizeInput) || sizeInput == "") {
+    var input = document.getElementById('input').value
+    inputstring = toString(input);
+    console.log(input)
+    if (sizeInput == "" || input == "") {
+        alert("fields are empty.")
+    } else if (isNaN(sizeInput)) {
         alert('please write a number');
     } else if (0 > sizeInput || sizeInput > 20) {
         alert('please include a size for your wordsearch between 1-20');
     } else {
         drawGrid(sizeInput);
+        var wordArray = wordListGen();
+        console.log(wordArray);
     }
 })
 
 function wordListGen() {
-    input = document.getElementById('input').value
-    input.split(",");
-    return input;
+    var input = document.getElementById('input').value
+    
+    var output;
+    if (input != "") {
+    output = input.split(",");
+    }
+    return output;
 }
 
 function drawGrid(sizeInput) {
 
-   
+   container.innerHTML = "";
 
     for(i=0; i<sizeInput; i++) {
         var row = document.createElement('div');
