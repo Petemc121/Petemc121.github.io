b732ff4f36450f3e10496d88bf3ae38f23665fd6
Peter McNulty <67034896+Petemc121@users.noreply.github.com>
2021-03-12 21:05:40 +0900

    Update wordsearch.js

--- wordsearch.js
+++ wordsearch.js
@@ -1,5 +1,5 @@
 
-var container = document.getElementById('wordsearch-container');
+var container = document.getElementById('wordsearch_container');
 var makeButton = document.getElementById('make')
 var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
@@ -15,12 +15,21 @@ make.addEventListener('click', function() {
         alert("fields are empty.")
     } else if (isNaN(sizeInput)) {
         alert('please write a number');
-    } else if (0 > sizeInput || sizeInput > 20) {
-        alert('please include a size for your wordsearch between 1-20');
+    } else if (8 > sizeInput || sizeInput > 20) {
+        alert('please include a size for your wordsearch between 8-20');
     } else {
         drawGrid(sizeInput);
         var wordArray = wordListGen();
-        console.log(wordArray);
+        for(i=0;i<wordArray.length;i++) {
+            random = Math.floor(Math.random()*sizeInput);
+            let row = document.getElementById(random);
+            for(j=0;j<wordArray[i].length;j++) {
+                
+                    let box = document.getElementById(row.id + j);
+                    console.log(box.id)
+                    box.innerHTML = wordArray[i][j];
+            }
+        }
     }
 })
 
@@ -40,7 +49,7 @@ function drawGrid(sizeInput) {
 
     for(i=0; i<sizeInput; i++) {
         var row = document.createElement('div');
-        row.setAttribute('id', "box" + i);
+        row.setAttribute('id',  i);
         row.setAttribute('class', "row")
         container.appendChild(row)
 
@@ -49,7 +58,7 @@ function drawGrid(sizeInput) {
         {
             letter = Math.floor(Math.random() * 26);
             var box = document.createElement('div');
-            box.setAttribute('id', row.id + j);
+            box.setAttribute('id', "" + i + j);
             box.setAttribute('class', 'box');
             box.innerHTML = alphabet[letter];
             row.appendChild(box);
